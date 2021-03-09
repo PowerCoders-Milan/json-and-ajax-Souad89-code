34505c7128584403887fe6a6b4b158cbd5cca78c
Souad Allahham <souadallahham89@gmail.com>
2021-03-09 12:29:25 +0100

    I finished ex2

---
+++ ex2/exercise.js
@@ -0,0 +1,7 @@
+/*Let’s call the placeholder API like in the slides and let’s show the information that we receive in the html.*/
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  
