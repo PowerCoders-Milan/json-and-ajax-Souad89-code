0e3366a99822a82a83d3b8b02562a1cb77becdb3
Souad Allahham <souadallahham89@gmail.com>
2021-03-12 12:29:04 +0100

    I updated and fixed ex2

--- ex2/exercise.js
+++ ex2/exercise.js
@@ -2,6 +2,24 @@
 
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    let body = document.querySelector('body');
+    let todoList = document.createElement('div');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let p4 = document.createElement('p');
+        body.appendChild(todoList);
+        todoList.appendChild(p1);
+        todoList.appendChild(p2);
+        todoList.appendChild(p3);
+        todoList.appendChild(p4);
+        p1.innerHTML= `Have you finished this task? ${data.completed}`
+        p2.innerHTML= `ID : ${data.id}`
+        p3.innerHTML= `The task : ${data.title}`
+        p4.innerHTML= `User ID : ${data.userid}`
+        console.log(data);
+  })
+    
 
   
